6c94b87ffe5bf34bf29fca6620bf23ec392af795
V1shwanath <j.vish2002@gmail.com>
2024-04-01 23:50:43 +0530

    commit

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
-PIP = ./.venv/Scripts/pip
+PIP = .\.venv\Scripts\pip
 PYTHON = ./.venv/Scripts/python
 
 
 
 install:
 	python -m venv .venv && \
-		. ./.venv/Scripts/activate && \
-		$(PIP) install -r requirements/requirements-gpu.txt
+		.\.venv\Scripts\activate && \
+		$(PIP) install -r requirements\requirements-gpu.txt
 
 #format all python documents in the project
 format:
-	$(PYTHON) -m black *.py
+	$(PYTHON) -m black tests/ *.py
+
+test:
+	$(PYTHON) -m pytest tests/ 
+
+lint:
+	$(PYTHON) -m pylint tests/ *.py
 
 clean:
 	rm -rf .venv
--- tests/test_similarity.py
+++ tests/test_similarity.py
@@ -5,23 +5,22 @@ from models.rnn import manhattan_similarity
 
 
 class TestSimilarity(tf.test.TestCase):
-
     def testManhattanSimilaritySame(self):
         with self.cached_session() as test_session:
-            x1 = np.array([[1., 1.]])
-            x2 = np.array([[1., 1.]])
+            x1 = np.array([[1.0, 1.0]])
+            x2 = np.array([[1.0, 1.0]])
             siamese_lstm_model = manhattan_similarity(x1, x2)
 
             actual_output = test_session.run(siamese_lstm_model)
-            correct_output = [1.]
+            correct_output = [1.0]
             self.assertEqual(actual_output, correct_output)
 
     def testSimilarity2D(self):
         with self.cached_session() as test_session:
-            x1 = np.array([[1., 1.], [1., 1.]])
-            x2 = np.array([[1., 1.], [1., 1.]])
+            x1 = np.array([[1.0, 1.0], [1.0, 1.0]])
+            x2 = np.array([[1.0, 1.0], [1.0, 1.0]])
             siamese_lstm_model = manhattan_similarity(x1, x2)
 
             actual_output = test_session.run(siamese_lstm_model)
-            correct_output = [[1.], [1.]]
+            correct_output = [[1.0], [1.0]]
             self.assertAllEqual(actual_output, correct_output)
